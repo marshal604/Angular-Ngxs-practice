8057f4a09a9acfc6a19386539ec2a02ed2df64e7
Lerdor <marshal604@gmail.com>
2018-04-01 12:47:04 +0800

    [Issue]  Ngxs練習 [Detail] 定義Todo state 與 創建 Todo Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-
+import {NgxsModule} from '@ngxs/store';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { TodoComponent } from './todo/todo.component';
+import {TodoState} from './todo/todo.state';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxsModule.forRoot([TodoState])
   ],
   providers: [],
   bootstrap: [AppComponent]
